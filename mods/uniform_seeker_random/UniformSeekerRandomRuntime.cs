e4fe323e7e5e30fe6e44f03a755357199ee70e3d
chelokot <andrey.vlasenko.work@gmail.com>
2026-03-31 23:44:29 +0300

    Fix uniform seeker recursion

--- mods/uniform_seeker_random/UniformSeekerRandomRuntime.cs
+++ mods/uniform_seeker_random/UniformSeekerRandomRuntime.cs
@@ -1,12 +1,21 @@
 using BepInEx.Logging;
 using Gameplay.Match.MatchState;
+using Gameplay.Player.Components;
 using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
 using Types;
 
 namespace SneakOut.UniformSeekerRandom;
 
 internal static class UniformSeekerRandomRuntime
 {
+    private static readonly System.Random Random = new();
+    private static readonly FieldInfo? NetworkPlayerRegistryComponentsField = AccessTools.Field(typeof(NetworkPlayerRegistry), "_components");
+    private static readonly Type? ShouldStartStateClosureType = typeof(ShouldStartState).GetNestedType("<>c", BindingFlags.NonPublic);
+    private static readonly FieldInfo? ShouldStartStateClosureInstanceField = ShouldStartStateClosureType?.GetField("<>9", BindingFlags.Public | BindingFlags.Static);
+    private static readonly MethodInfo? ShouldStartStateGetRandomSeekerPredicateMethod = ShouldStartStateClosureType?.GetMethod("<GetRandomSeeker>b__9_0", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
     private static ManualLogSource? _logger;
     private static Harmony? _harmony;
     private static UniformSeekerRandomConfig? _configuration;
@@ -31,13 +40,70 @@ internal static class UniformSeekerRandomRuntime
             return false;
         }
 
-        result = shouldStartState.RandomizeSeeker();
+        var candidateInternalIds = CollectEligibleSeekerInternalIds(shouldStartState);
+        if (candidateInternalIds.Count == 0)
+        {
+            return false;
+        }
+
+        result = candidateInternalIds[Random.Next(candidateInternalIds.Count)];
 
         if (_configuration.EnableLogging.Value)
         {
-            _logger?.LogInfo($"Uniform seeker random override selected seeker index {result}");
+            _logger?.LogInfo($"Uniform seeker random override selected seeker id {result} from {candidateInternalIds.Count} candidates");
         }
 
         return true;
+    }
+
+    private static List<int> CollectEligibleSeekerInternalIds(ShouldStartState shouldStartState)
+    {
+        var predicate = GetOriginalGetRandomSeekerPredicate();
+        var candidateInternalIds = new List<int>();
+        var networkPlayers = NetworkPlayerRegistryComponentsField?.GetValue(shouldStartState._networkPlayerRegistry) as SpookedNetworkPlayer[];
+        if (networkPlayers is null)
+        {
+            return candidateInternalIds;
+        }
+
+        foreach (var networkPlayer in networkPlayers)
+        {
+            if (networkPlayer is null)
+            {
+                continue;
+            }
+
+            if (predicate is not null)
+            {
+                if (!predicate(networkPlayer))
+                {
+                    continue;
+                }
+            }
+            else if (!networkPlayer.CanBeSeeker)
+            {
+                continue;
+            }
+
+            candidateInternalIds.Add(networkPlayer.InternalId);
+        }
+
+        return candidateInternalIds;
+    }
+
+    private static Func<SpookedNetworkPlayer, bool>? GetOriginalGetRandomSeekerPredicate()
+    {
+        if (ShouldStartStateClosureInstanceField is null || ShouldStartStateGetRandomSeekerPredicateMethod is null)
+        {
+            return null;
+        }
+
+        var closureInstance = ShouldStartStateClosureInstanceField.GetValue(null);
+        if (closureInstance is null)
+        {
+            return null;
+        }
+
+        return networkPlayer => (bool)ShouldStartStateGetRandomSeekerPredicateMethod.Invoke(closureInstance, new object[] { networkPlayer })!;
     }
 }

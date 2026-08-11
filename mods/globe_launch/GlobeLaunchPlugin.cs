b56c97a293b58fad1cab1b9755797fdd7bfd8565
Artem Prokop <artem13.prokop@gmail.com>
2026-08-11 17:25:12 +0200

    chore: bump plugin version to 0.1.1

--- mods/globe_launch/GlobeLaunchPlugin.cs
+++ mods/globe_launch/GlobeLaunchPlugin.cs
@@ -8,7 +8,7 @@ public sealed class GlobeLaunchPlugin : BasePlugin
 {
     public const string PluginGuid = "chelokot.sneakout.globe-launch";
     public const string PluginName = "Globe Launch";
-    public const string PluginVersion = "0.1.0";
+    public const string PluginVersion = "0.1.1";
 
     public override void Load()
     {
